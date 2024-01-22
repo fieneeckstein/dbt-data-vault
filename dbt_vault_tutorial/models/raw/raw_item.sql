b295368a6d5d3d58fb1e894f53898b3a5d93564d
Fiene <f.eckstein@ostfalia.de>
2024-01-22 11:33:36 +0100

    add nk to raw stage

--- dbt_vault_tutorial/models/raw/raw_item.sql
+++ dbt_vault_tutorial/models/raw/raw_item.sql
@@ -1,6 +1,13 @@
 with item_data as (
     select *, current_timestamp as load_date from {{ source('bikerpoint','item') }}
+),
+purchase_nk as (
+    select id as purId, orderno from  {{ source('bikerpoint','purchase') }} 
+),
+product_nk as (
+    select id as pid, ean from  {{ source('bikerpoint','product') }} 
+),
+final as (
+    select * from item_data i left join purchase_nk purNk on i.order_id = purNk.purId left join product_nk pnk on i.product_id = pnk.pid
 )
-select * from item_data
-
--- Reichere item noch mit dem NK der Purchases und Produkte an
+select * from final
--- dbt_vault_tutorial/models/raw/raw_purchase.sql
+++ dbt_vault_tutorial/models/raw/raw_purchase.sql
@@ -1,5 +1,11 @@
+
 with purchase_data as (
     select *, current_timestamp as load_date from {{ source('bikerpoint','purchase') }}
+),
+customer_nk as (
+    select id as cid, customerno from  {{ source('bikerpoint','customer') }} 
+),
+final as (
+    select * from purchase_data p left join customer_nk cnk on p.customer_id = cnk.cid
 )
-
-select * from purchase_data
+select * from final
