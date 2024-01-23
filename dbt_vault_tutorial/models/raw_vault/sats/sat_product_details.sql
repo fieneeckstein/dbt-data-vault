e8e11228172fcdde861bdf96e050d8aa8809b101
Fiene <f.eckstein@ostfalia.de>
2024-01-23 08:39:13 +0100

    load satellites

---
+++ dbt_vault_tutorial/models/raw_vault/sats/sat_product_details.sql
@@ -0,0 +1,25 @@
+{%- set yaml_metadata -%}
+source_model: "stg_products"
+src_pk: "PRODUCT_PK"
+src_hashdiff: 
+  source_column: "PRODUCT_HASHDIFF"
+  alias: "HASHDIFF"
+src_payload:
+  - "ean"
+  - "airpressure"
+  - "operation"
+  - "price"
+src_eff: "EFFECTIVE_FROM"
+src_ldts: "LOAD_DATE"
+src_source: "RECORD_SOURCE"
+{%- endset -%}
+
+{% set metadata_dict = fromyaml(yaml_metadata) %}
+
+{{ automate_dv.sat(src_pk=metadata_dict["src_pk"],
+                   src_hashdiff=metadata_dict["src_hashdiff"],
+                   src_payload=metadata_dict["src_payload"],
+                   src_eff=metadata_dict["src_eff"],
+                   src_ldts=metadata_dict["src_ldts"],
+                   src_source=metadata_dict["src_source"],
+                   source_model=metadata_dict["source_model"])   }}
