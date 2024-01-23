6d641a9c0936a9ab57c2f4a9a73e1ff5d9676684
Fiene <f.eckstein@ostfalia.de>
2024-01-23 14:57:33 +0100

    load dim product

---
+++ dbt_vault_tutorial/models/star/dim_product.sql
@@ -0,0 +1,9 @@
+with active_satellite_entry as (
+select product_pk as active_ppk, sat_product_details_ldts as active_ldts from {{ ref('pit_product') }} where  as_of_date  = current_date + 1
+),
+
+final as (
+    select product_pk, ean, price from {{ ref('sat_product_details') }} join active_satellite_entry a on product_pk=active_ppk and load_date = active_ldts
+)
+
+select * from final
