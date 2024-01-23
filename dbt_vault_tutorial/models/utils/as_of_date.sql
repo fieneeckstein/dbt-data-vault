dbe1d20abae43bcfecca69f4f71fc0d4b2bdc689
Fiene <f.eckstein@ostfalia.de>
2024-01-23 14:39:44 +0100

    update as_od_date

--- dbt_vault_tutorial/models/utils/as_of_date.sql
+++ dbt_vault_tutorial/models/utils/as_of_date.sql
@@ -1,8 +1,8 @@
 {{ config(materialized='table') }}
 
-{%- set datepart = "minute" -%}
-{%- set start_date = "TO_DATE('2023-12-20', 'yyyy-mm-dd')" -%}
-{%- set end_date = "TO_DATE('2023-12-21', 'yyyy-mm-dd')" -%}
+{%- set datepart = "day" -%}
+{%- set start_date = "TO_DATE('2024-01-01', 'yyyy-mm-dd')" -%}
+{%- set end_date = "TO_DATE('2024-12-31', 'yyyy-mm-dd')" -%}
 
 WITH as_of_date AS (
     {{ dbt_utils.date_spine(datepart=datepart, 
