d60da2811855416c5f420a83c84fc1c9e347f9f6
Fiene Eckstein <f.eckstein@ostfalia.de>
2024-01-24 13:16:47 +0000

    Update as_of_date.sql

--- dbt_vault_tutorial/models/utils/as_of_date.sql
+++ dbt_vault_tutorial/models/utils/as_of_date.sql
@@ -1,7 +1,7 @@
 {{ config(materialized='table') }}
 
 {%- set datepart = "day" -%}
-{%- set start_date = "TO_DATE('2024-01-01', 'yyyy-mm-dd')" -%}
+{%- set start_date = "TO_DATE('2022-01-01', 'yyyy-mm-dd')" -%}
 {%- set end_date = "TO_DATE('2024-12-31', 'yyyy-mm-dd')" -%}
 
 WITH as_of_date AS (
